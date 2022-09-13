af1c074eb07257cba1784846ca524c247a7088cc
3amrabdelwahab <3amr.abdelwahab@gmail.com>
2022-09-13 23:49:06 +0200

    mob

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-
 // alert('alert');
 
+// Variable
+let ham = document.getElementById('ham');
+let links = document.getElementById('links');
+
+ham.addEventListener('click', hamClick);
+
+function hamClick () {
+    ham.classList.toggle('active');
+    links.classList.toggle('show');
+}
+
+
+let itemS = document.getElementById('links').getElementsByTagName('li');
+console.log(itemS);
+
+for ( i = 0 ; i < itemS.length ; i++) {
+    itemS[i].addEventListener('click', timing);
+}
+
+function timing () {
+    setTimeout(hamClick, 400);
+}
