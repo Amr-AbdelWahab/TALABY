1287dfc32d53f7b306f0d78fcddbb43861cc6894
3amrabdelwahab <3amr.abdelwahab@gmail.com>
2022-09-17 15:05:23 +0200

    next page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 // alert('alert');
 
+// include
+function load(target, url) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.onreadystatechange = function () {
+        if (r.readyState != 4 || r.status != 200) return;
+        target.innerHTML = r.responseText;
+    };
+    r.send();
+}
+
+let includes = Array.from(document.querySelectorAll('[data-include]'));
+includes.map(include => {
+    let file = include.dataset['include'] + '.html';
+    load(include, file);
+});
+
+
+
+
 // Variable
 let ham = document.getElementById('ham');
 let links = document.getElementById('links');
@@ -9,6 +29,7 @@ ham.addEventListener('click', hamClick);
 function hamClick () {
     ham.classList.toggle('active');
     links.classList.toggle('show');
+
 }
 
 
@@ -22,3 +43,5 @@ for ( i = 0 ; i < itemS.length ; i++) {
 function timing () {
     setTimeout(hamClick, 400);
 }
+
+
