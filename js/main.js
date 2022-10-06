e93354081a2528553de657d29c44fb077154fe1d
3amrabdelwahab <3amr.abdelwahab@gmail.com>
2022-10-06 17:02:53 +0200

    validation

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 // alert('alert');
 
 // include
-function load(target, url) {
-    var r = new XMLHttpRequest();
-    r.open("GET", url, true);
-    r.onreadystatechange = function () {
-        if (r.readyState != 4 || r.status != 200) return;
-        target.innerHTML = r.responseText;
-    };
-    r.send();
-}
+// function load(target, url) {
+//     var r = new XMLHttpRequest();
+//     r.open("GET", url, true);
+//     r.onreadystatechange = function () {
+//         if (r.readyState != 4 || r.status != 200) return;
+//         target.innerHTML = r.responseText;
+//     };
+//     r.send();
+// }
 
-let includes = Array.from(document.querySelectorAll('[data-include]'));
-includes.map(include => {
-    let file = include.dataset['include'] + '.html';
-    load(include, file);
-});
+// let includes = Array.from(document.querySelectorAll('[data-include]'));
+// includes.map(include => {
+//     let file = include.dataset['include'] + '.html';
+//     load(include, file);
+// });
 
 
 
@@ -33,7 +33,7 @@ function hamClick () {
 };
 
 let itemS = document.getElementById('links').getElementsByTagName('li');
-console.log(itemS);
+// console.log(itemS);
 
 for ( i = 0 ; i < itemS.length ; i++) {
     itemS[i].addEventListener('click', timing);
@@ -46,25 +46,10 @@ function timing () {
 
 
 
-// let body = document.getElementsByTagName('div');
-// for ( i = 0 ; i < body.length ; i++) {
-//     body[i].addEventListener('click', timing5);
-// };
-
-// function timing5 () {
-//     setTimeout(hamOff, 400);
-// }
-
-
-// function hamOff () {
-//     ham.classList.remove('active');
-//     links.classList.remove('show');
-// };
-
 
 // الاسئلة الشائعة
 
-let qaContainer = document.getElementById('qa').getElementsByTagName('li');
+let qaContainer = document.querySelectorAll('#qa li')
 
 for ( i = 0 ; i < qaContainer.length ; i++) {
     qaContainer[i].addEventListener('click', showAnswer);
@@ -72,12 +57,38 @@ for ( i = 0 ; i < qaContainer.length ; i++) {
 
 function showAnswer () {
     
-    // for ( i = 0 ; i < qaContainer.length ; i++) {
-    //     qaContainer[i].classList.remove('showAnswer');
-    // };
+    for ( i = 0 ; i < qaContainer.length ; i++) {
+        qaContainer[i].classList.remove('showAnswer');
+    };
 
-
-    
     this.classList.toggle('showAnswer');
 };
 
+
+//---------
+//   submit
+//-----------
+
+//Variable
+// let submit = document.getElementById('submit');
+// let numMassage = document.getElementById('numMassage');
+// let maxInputNum = document.getElementById('maxInputNum');
+// let form = document.getElementById('form');
+
+// console.log(submit);
+
+let numError = document.getElementById('numError');
+console.log(numError);
+
+function validatePhonNum() {
+    let phoneNum = document.getElementById('phoneNum').value;
+    
+
+    if (phoneNum.length !== 10 ){
+        numError.innerHTML = '*ادخل رقم الجوال ( مكون من 10 أرقام )';
+        console.log(phoneNum);
+    }else{
+        numError.innerHTML = ""
+    }
+    
+}
