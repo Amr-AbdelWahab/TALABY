3c550d80c93b1bcc92fcedd01df1fa13135298ad
3amrabdelwahab <3amr.abdelwahab@gmail.com>
2022-10-17 07:11:17 +0200

    tracking

--- js/api.js
+++ js/api.js
@@ -1,13 +1,32 @@
-let baseUrl = "https://www.postman.com/collections/0e3d8ad92d8f43ed41d2"
-fetch(baseUrl)
+let baseUrl = "https://www.postman.com/collections/0e3d8ad92d8f43ed41d2";
+
+let packageTrackUrl = "https://apisv2.logestechs.com/api/guests/1/packages/tracking?barcode=";
+let userTrackingBarcode = document.getElementById('trackerID');
+
+
+fetch(packageTrackUrl + userTrackingBarcode.value)
 .then(function(response){
   return response.json();
 })
 .then(function(data){
-  console.log(data.item[3]);
-  let dfd = data.item[3];
-  console.log(dfd)
+  console.log(data.destinationAddress.arabicCityName);
+  console.log(data.senderFirstName + data.senderMiddleName);
+
+  // Barcode number
+document.getElementById('userBarcode').innerHTML = data.barcode;
+
+  // Sender Name
+  // let senderNameUser = data.senderFirstName +" "+ data.senderMiddleName;
+  document.getElementById('senderName').innerHTML = data.fullSenderName;
+
+  // Pack Info
+  document.getElementById('weight').innerHTML = data.width + " كجم";
+  document.getElementById('length').innerHTML = data.length + " سم";
+  document.getElementById('width').innerHTML = data.width + " سم";
+
 })
+
+
 
 
 
@@ -21,9 +40,21 @@ let closePopUp = document.getElementById('closePopUp');
 btnTrackMyPack.addEventListener('click', trackMyPack);
 function trackMyPack (){
   packageTracking.classList.remove('disNon');
+  setTimeout(scaleUp,50);
+  function scaleUp (){
+    packageTracking.classList.add('open');
+  }console.log(userTrackingBarcode.value);
+
 }
 //Close 
 closePopUp.addEventListener('click', close);
 function close (){
-  packageTracking.classList.add('disNon');
+  packageTracking.classList.remove('open');
+  setTimeout(scaleDn,200);
+  function scaleDn (){
+    packageTracking.classList.add('disNon');
+  }
 }
+
+// Barcode number
+document.getElementById('userBarcode').innerHTML = userTrackingBarcode.value;
